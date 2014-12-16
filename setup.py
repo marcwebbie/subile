3e6fa161dfc5c120060615e85042744d6b77b8da
marcwebbie <marcwebbie@gmail.com>
2014-12-16 01:23:42 -0300

    Up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 with open("README.rst") as f:
     long_description = f.read() + '\n'
